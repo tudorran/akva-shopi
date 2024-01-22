ecf65b1065d3998fe1e847d62efb98ff724eefd5
tudorran <132088453+tudorran@users.noreply.github.com>
2024-01-22 04:36:08 +0200

    Initialize summer-demo

---
+++ apps/storefront/app/components/overlays/VisualEditing.tsx
@@ -0,0 +1,16 @@
+// import { enableVisualEditing } from "@sanity/overlays";
+// import { useEffect } from "react";
+
+export default function VisualEditing() {
+  // useEffect(enableVisualEditing, []);
+
+  return null;
+  // return (
+  //   <div
+  //     className="pointer-events-none fixed inset-0 z-50 flex items-center justify-center bg-red/80 text-3xl text-white"
+  //     style={{ opacity: 0.5 }}
+  //   >
+  //     Visual Editing!
+  //   </div>
+  // );
+}
---
+++ apps/storefront/app/routes/_store.($lang).pages.$handle.tsx
@@ -0,0 +1,89 @@
+import { Await, useLoaderData, useParams } from "@remix-run/react";
+import type { SeoHandleFunction } from "@shopify/hydrogen";
+import {
+  defer,
+  type LoaderArgs,
+  type SerializeFrom,
+} from "@shopify/remix-oxygen";
+import clsx from "clsx";
+import { SanityPreview } from "hydrogen-sanity";
+import { Suspense } from "react";
+import invariant from "tiny-invariant";
+
+import PageHero from "~/components/heroes/Page";
+import PortableText from "~/components/portableText/PortableText";
+import type { SanityPage } from "~/lib/sanity";
+import { ColorTheme } from "~/lib/theme";
+import { fetchGids, notFound, validateLocale } from "~/lib/utils";
+import { PAGE_QUERY } from "~/queries/sanity/page";
+
+const seo: SeoHandleFunction<typeof loader> = ({ data }) => ({
+  title: data?.page?.seo?.title,
+  description: data?.page?.seo?.description,
+  media: data?.page?.seo?.image,
+});
+
+export const handle = {
+  seo,
+};
+
+export async function loader({ params, context }: LoaderArgs) {
+  validateLocale({ context, params });
+
+  const { handle } = params;
+  invariant(handle, "Missing page handle");
+
+  const cache = context.storefront.CacheCustom({
+    mode: "public",
+    maxAge: 60,
+    staleWhileRevalidate: 60,
+  });
+
+  const page = await context.sanity.query<SanityPage>({
+    query: PAGE_QUERY,
+    params: {
+      slug: handle,
+    },
+    cache,
+  });
+
+  if (!page) {
+    throw notFound();
+  }
+
+  // Resolve any references to products on the Storefront API
+  const gids = fetchGids({ page, context });
+
+  return defer({ page, gids });
+}
+
+export default function Page() {
+  const { page, gids } = useLoaderData<SerializeFrom<typeof loader>>();
+  const { handle } = useParams();
+
+  return (
+    <SanityPreview data={page} query={PAGE_QUERY} params={{ slug: handle }}>
+      {(page) => (
+        <ColorTheme value={page?.colorTheme}>
+          <Suspense>
+            <Await resolve={gids}>
+              {/* Page hero */}
+              <PageHero fallbackTitle={page?.title || ""} hero={page?.hero} />
+              {/* Body */}
+              {page?.body && (
+                <PortableText
+                  blocks={page.body}
+                  centered
+                  className={clsx(
+                    "mx-auto max-w-[660px] px-4 pb-24 pt-8", //
+                    "md:px-8"
+                  )}
+                />
+              )}
+            </Await>
+          </Suspense>
+        </ColorTheme>
+      )}
+    </SanityPreview>
+  );
+}
